1bcdcdaa674d8725e2a4ef828bd0b7c4bd529bf2
lheapcs <lheap_cs@protonmail.com>
2022-05-14 14:29:21 +0100

    Fixed footer on homepage. Made some further QOL changes.

--- footer.php
+++ footer.php
@@ -1,13 +1,12 @@
 
     <footer>
-    <picture><a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-html32" height="31" width="88"></a></picture>
+        <picture><a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-html32" height="31" width="88"></a></picture>
         <small>
             <address>
                 <a href="mailto:adavey508@gmail.com">Contact Us</a>
             </address>
         </small>
-    </footer>   
-    
+    </footer> 
 </div>
 </body>
 </html>
--- header.php
+++ header.php
@@ -13,76 +13,77 @@
 </head>
 
 <body>
+<div id ="page-container">
+    <div id="content-wrap">
+        <nav>
+            <div class="wrapper">
+                <ul>
+                    <li><button onclick="window.location.href='index.php';">Home</button></li>
+                    <?php
+                    if (isset($_SESSION["userid"])) {
+                        echo "<li><a href='profile.php'>Profile</a></li>";
+                        echo "<li><a href='logout.php'>Log out</a></li>";
+                    }
+                    else {
+                        echo "<li><button onclick=\"document.getElementById('sign').style.display='block'\">Sign Up</button></li>";
+                        echo "<li><button onclick=\"document.getElementById('log').style.display='block'\">Login</button></li>";
+                    }
+                    ?>
+                </ul>
+            </div>
+        </nav>
 
-<nav>
-    <div class="wrapper">
-        <ul>
-            <li><a href="index.php">Home Page</a></li>
-            <?php
-                if (isset($_SESSION["userid"])) {
-                    echo "<li><a href='profile.php'>Profile</a></li>";
-                    echo "<li><a href='logout.php'>Log out</a></li>";
-                }
-                else {
-                    echo "<li><button onclick=\"document.getElementById('sign').style.display='block'\">Sign Up</button></li>";
-                    echo "<li><button onclick=\"document.getElementById('log').style.display='block'\">Login</button></li>";
-                }
-            ?>
-        </ul>
-    </div>
-</nav>
+        <div id="sign" class="formbox">
+        <!-- <span onclick="document.getElementById('sign').style.display='none'" class="close" title="Close">X</span> (if an X is wanted instead)-->
+            <button onclick="document.getElementById('sign').style.display='none'" class="close-button" id="close-button">Close</button>
+            <form class="form-content" action="processes/signup.fnc.php" method="POST">
+            <div class="fields">
+                <h1>Sign Up</h1>
+                <input type="text" name="name" placeholder="Full name (optional)">
+                <input type="text" name="email" placeholder="Email" required>
+                <input type="text" name="uname" placeholder="User name" required>
+                <input type="password" name="pwd" placeholder="Password" required>
+                <input type="password" name="pwdrepeat" placeholder="Repeat password" required>
+                <p>Creation of an account is an acceptance of our <a href ="privacypolicy.html">Terms and Privacy</a> </p>
 
-<div id="sign" class="formbox">
-   <!-- <span onclick="document.getElementById('sign').style.display='none'" class="close" title="Close">X</span> (if an X is wanted instead)-->
-   <button onclick="document.getElementById('sign').style.display='none'" class="close-button" id="close-button">Close</button>
-    <form class="form-content" action="processes/signup.fnc.php" method="POST">
-        <div class="fields">
-            <h1>Sign Up</h1>
-            <input type="text" name="name" placeholder="Full name (optional)">
-            <input type="text" name="email" placeholder="Email" required>
-            <input type="text" name="uname" placeholder="User name" required>
-            <input type="password" name="pwd" placeholder="Password" required>
-            <input type="password" name="pwdrepeat" placeholder="Repeat password" required>
-            <p>Creation of an account is an acceptance of our <a href ="privacypolicy.html">Terms and Privacy</a> </p>
-
-            <div class="buttons">
-                <button type="button" onclick="document.getElementById('sign').style.display='none'" class="cancelbtn">Cancel</button>
-                <button type="submit" class="signupbtn">Sign Up</button>
-            </div> 
+                <div class="buttons">
+                    <button type="button" onclick="document.getElementById('sign').style.display='none'" class="cancelbtn">Cancel</button>
+                    <button type="submit" class="signupbtn">Sign Up</button>
+                </div> 
+            </div>
+            </form>
         </div>
-    </form>
-</div>
 
-<div id="log" class="formbox">
-    <button onclick="document.getElementById('log').style.display='none'" class="close-button" id="close-button">Close</button>
-    <form action="processes/login.fnc.php" method="POST">
-        <div class="fields">
-            <h1>Login</h1>
-            <input type="text" name="name" placeholder="Username/Email">
-            <input type="password" name="pwd" placeholder="Password">
+        <div id="log" class="formbox">
+            <button onclick="document.getElementById('log').style.display='none'" class="close-button" id="close-button">Close</button>
+            <form action="processes/login.fnc.php" method="POST">
+            <div class="fields">
+                <h1>Login</h1>
+                <input type="text" name="name" placeholder="Username/Email">
+                <input type="password" name="pwd" placeholder="Password">
 
-            <div class="buttons">
-                <button type="button" onclick="document.getElementById('log').style.display='none'" class="cancelbtn">Cancel</button>
-                <button type="submit" class="loginbtn">Login</button>
-            </div> 
+                <div class="buttons">
+                    <button type="button" onclick="document.getElementById('log').style.display='none'" class="cancelbtn">Cancel</button>
+                    <button type="submit" class="loginbtn">Login</button>
+                </div> 
+            </div>
+            </form>
         </div>
-    </form>
-</div>
+            
 
+        <script>
+        // Get the two formboxes
+        var login = document.getElementById('log');
+        var signup = document.getElementById('sign');
 
-<script>
-// Get the two formboxes
-var login = document.getElementById('log');
-var signup = document.getElementById('sign');
-
-// Close the boxes if the user clicks anywhere outside of them
-window.onclick = function(event) {
-    if (event.target == login) {
-        login.style.display = "none";
-    }
-    if (event.target == signup) {
-        signup.style.display = "none";
-    }
-}
-</script> 
-<div class="wrapper">
+        // Close the boxes if the user clicks anywhere outside of them
+        window.onclick = function(event) {
+            if (event.target == login) {
+                login.style.display = "none";
+            }
+            if (event.target == signup) {
+                signup.style.display = "none";
+            }
+        }
+        </script> 
+    <!--<div class="wrapper">-->
